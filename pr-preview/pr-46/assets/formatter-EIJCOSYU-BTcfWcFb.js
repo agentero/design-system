50ec09a9c0a3a53598acc700e26c05db7a8054d2
rafael-moro-ag <rafael-moro-ag@users.noreply.github.com>
2026-06-15 09:00:43 +0000

    Deploy preview for PR 46 🛫

---
+++ pr-preview/pr-46/assets/formatter-EIJCOSYU-BTcfWcFb.js
@@ -0,0 +1,1 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{f as t,p as n}from"./iframe-CyssEAJ1.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-CR5p8PX-.js";var s;e((()=>{a(),i(),n(),s=(0,t(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
